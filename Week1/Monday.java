e76716510e450b760a2bf87459112002a06f7675
Nolan Stutelberg <nstutelberg@gmail.com>
2023-08-21 19:55:28 -0500

    add monday files

---
+++ Week1/Monday.java
@@ -0,0 +1,20 @@
+package Week1;
+
+// Public means that the method is available to all other modules in the project
+// static means that there is no other outside data that is needed to be passed through the method. If you are passing parameters into the method, it wouldn't be static
+// The example below is passing just an array of strings into the main function
+// Every file has to have a class and that class has to be the same as the file name. And within the class, all code has to be nested in a method
+
+public class Monday {
+    /***
+     * Task 1 - Just do a simple hello world. Note that for the code to be executable, there needs to be a main method in the class definition
+     * @param args
+     */
+    public static void helloWorld (String[] args) {
+        System.out.println("Hello World" + '\n');
+    }
+
+    public static void main (String[] args) {
+        helloWorld(args);
+    }
+}
